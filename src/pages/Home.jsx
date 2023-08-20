0a4df58d7df99644304124683d1ab5f2beb5f50a
arnavco9573 <arnavsharm589@gmail.com>
2023-08-21 02:14:33 +0530

    I have removed API call as its give error message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,48 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Spinner from '../components/Spinner';
 import Product from '../components/Product';
+import { products as localData } from '../data.js';
+
+// const API_URL = 'https://api.escuelajs.co/api/v1/products';
 
 const Home = () => {
-    const API_URL = 'https://fakestoreapi.com/products';
     const [loading, setLoading] = useState(false);
     const [posts, setPosts] = useState([]);
+
     async function fetchProductData() {
         setLoading(true);
         try {
-            const res = await fetch(API_URL);
-            const data = await res.json();
-            setPosts(data);
+            // Fetching data directly from the imported localData
+            setPosts(localData);
         } catch (error) {
-            console.log("Error ");
+            console.error("Error fetching data:", error);
             setPosts([]);
         }
         setLoading(false);
     }
+    
 
     useEffect(() => {
         fetchProductData();
-    }, [])
+    }, []);
 
     return (
         <div>
-    {loading ? <Spinner /> : (
-        <div>
-            {posts.length > 0 ? (
-                <div className='grid  xl:grid-cols-4 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 gap-4 min-h-[80vh]'>
-                    {posts.map((post) => (
-                        <Product key={post.id} post={post} />
-                    ))}
-                </div>
+            {loading ? (
+                <Spinner />
             ) : (
-                <div className='flex justify-center items-center'>
-                    <p>No data Found</p>
+                <div>
+                    {posts.length > 0 ? (
+                        <div className='grid  xl:grid-cols-4 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 gap-4 min-h-[80vh]'>
+                            {posts.map((post) => (
+                                <Product key={post.id} post={post} />
+                            ))}
+                        </div>
+                    ) : (
+                        <div className='flex justify-center items-center'>
+                            <p>No data Found</p>
+                        </div>
+                    )}
                 </div>
             )}
         </div>
-    )}
-</div>
-
-    )
+    );
 };
 
-export default Home; 
+export default Home;
